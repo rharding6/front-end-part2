94009b7a888da2a26583eb95e5bdd0db9d93b0d6
Robert Harding <robisonfire@gmail.com>
2016-08-16 08:48:14 -0600

    added link to the style sheet

--- index.php
+++ index.php
@@ -2,9 +2,18 @@
 	<html>
 	<head>
 
+		<link href="css/style.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
+		<header>
 
+		</header>
+		<main>
+
+		</main>
+		<footer>
+
+		</footer>
 	</body>
 
 	</html>
