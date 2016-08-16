4633072219b9a6d0717d996d81434fd1f0e8f90b
Robert Harding <robisonfire@gmail.com>
2016-08-16 08:51:44 -0600

    added bootstrap

--- index.php
+++ index.php
@@ -1,15 +1,58 @@
 <!DOCTYPE html>
 	<html>
 	<head>
+		<meta charset="utf-8" />
+		<!--this makes the site compatable with internet explorerer sites-->
+		<meta http-equiv="x-ua-compatible" content="IE=edge"/>
+		<!--
+		* this says that the width of the page is going to have a 1-1 relationship to the viewport
+		* it would probably work anyway, but it assures that nothing odd will happen to the viewport
+		* across different devices
+		*-->
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
+		<!-- Latest compiled and minified CSS copied and pasted from getboostrap.com-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!--add other css files here-->
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		//custom css
 		<link href="css/style.css" rel="stylesheet" type="text/css">
+
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!--jquery found at https://cdnjs.com/libraries/jquery-->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"/>
+
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	</head>
 	<body>
 		<header>
 
 		</header>
 		<main>
+			<div class="container">
+				<div class="row">
 
+				</div>
+				<div class="row">
+
+				</div>
+			</div>
+			<div class="container">
+
+			</div>
 		</main>
 		<footer>
 
